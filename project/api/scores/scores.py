44601e980f9c2958350cef3b3af95024a1edd96c
Dmitry Sichkar <dmmeteo@gmail.com>
2018-07-10 18:56:35 +0300

    refactor via black

--- project/api/scores/scores.py
+++ project/api/scores/scores.py
@@ -9,24 +9,19 @@ from project.api.scores.models import Score
 scores_blueprint = Blueprint("scores", __name__)
 
 
-@scores_blueprint.route('/scores', method=['GET'])
+@scores_blueprint.route("/scores", method=["GET"])
 def get_all_scores():
     """Get all scores"""
     scores = Score.query.all()
     scores_list = []
     for score in scores:
         score_object = {
-            'id': score.id,
-            'user_id': score.user_id,
-            'exercise_id': score.exercise_id,
-            'created_at': score.created_at,
-            'updated_at': score.updated_at
+            "id": score.id,
+            "user_id": score.user_id,
+            "exercise_id": score.exercise_id,
+            "created_at": score.created_at,
+            "updated_at": score.updated_at,
         }
         scores_list.append(score_object)
-    response_object = {
-        'status': 'success',
-        'data': {
-            'scores': scores_list
-        }
-    }
+    response_object = {"status": "success", "data": {"scores": scores_list}}
     return jsonify(response_object), 200
